bdee4cef65a1b7597a623f54a2edb64543df802a
ifatdoctorwali <ifathuzaifa1997@gmail.com>
2025-02-04 09:17:40 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <titale>Payment Platform</title>
+</head>
+<body>
+    <h1>Welcome to PayPal-inspired Platform</h1>
+    <a href="login.php">Login</a> | <a href="register.php">Register</a>
+</body>
+</html>
